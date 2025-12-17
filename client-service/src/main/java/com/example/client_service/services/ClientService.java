0f02f5e7fce709bf477c2e476f113a59320d0e1c
Mohamed Amine ELIDRISSI <130193461+Mohamedamine88@users.noreply.github.com>
2025-12-17 21:39:44 +0100

    Add files via upload

---
+++ client-service/src/main/java/com/example/client_service/services/ClientService.java
@@ -0,0 +1,31 @@
+package com.example.client_service.services;
+
+import com.example.client_service.entities.Client;
+import com.example.client_service.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+
+    public List<Client> findAll() {
+        return clientRepository.findAll();
+    }
+
+    public Client findById(Long id) throws Exception {
+        return clientRepository.findById(id)
+                .orElseThrow(() -> new Exception("Client non trouvé avec l'ID: " + id));
+    }
+
+
+    public Client addClient(Client client) {
+        return clientRepository.save(client);
+    }
+
+}
